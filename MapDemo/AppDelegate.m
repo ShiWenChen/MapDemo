d396001b8862a6a72b74e4fa013dfd72b5c415a6
ShiWenChen <cswzmg@live.com>
2016-10-09 18:15:04 +0800

    添加高德SDK，添加所需部分图片

--- MapDemo/AppDelegate.m
+++ MapDemo/AppDelegate.m
@@ -16,7 +16,7 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+    [AMapServices sharedServices].apiKey = MapKey;
     return YES;
 }
 
